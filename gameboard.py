d21c64335dfbcf65a6024f1f0e60f336b62018a1
Show WaiYan <showwaiyan555@gmail.com>
2022-01-23 00:44:25 +0630

    Add checking square function

--- gameboard.py
+++ gameboard.py
@@ -1,7 +1,19 @@
 class Board:
     board = [0,0,0,0,0,0,0,0,0]
-
+  
     def __init__(self,screen_size):
         self.screen_size = screen_size
+    
+    def check_square(self,mouse_pos):
+        # Checking mouse positio is in which square
+
+        if mouse_pos[1] <= 200:
+            if mouse_pos[0] <= 200:
+                self.board[0] = 1
+            elif mouse_pos[0] <= 420:
+                self.board[1] = 1
+            elif mouse_pos[0] <= 640:
+                self.board[2] = 1
 
 
+            
--- main.py
+++ main.py
@@ -12,9 +12,12 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            board.check_square(event.pos)
 
     screen.fill((255,255,255))
-    
+
+    #Drawing nine square on the screen
     for y in range(0,board.screen_size[1],220):
         for x in range(0,board.screen_size[0],220):
             pygame.draw.rect(screen, (0,0,0), ((x,y),(200,200)))
