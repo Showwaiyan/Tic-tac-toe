df3f8f2dfbcba34dd56a207978859be54de095ef
Show WaiYan <showwaiyan555@gmail.com>
2022-01-24 11:47:56 +0630

    Final debugged circle and cross adding feature

--- gameboard.py
+++ gameboard.py
@@ -1,5 +1,7 @@
 class Board:
-    board = [0,0,0,0,0,0,0,0,0]
+    board = [[0,0,0]
+            ,[0,0,0]
+            ,[0,0,0]]
 
     def __init__(self,screen_size):
         self.screen_size = screen_size
@@ -7,44 +9,19 @@ class Board:
     def change_square(self,mouse_pos, char):
         # Checking mouse positio is in which square
 
-        if mouse_pos[1] <= 200:
-            if mouse_pos[0] <= 200:
-                self.board[0] = char if self.board[0] == 0 else self.board[0]
-            elif mouse_pos[0] <= 420:
-                self.board[1] = char if self.board[1] == 0 else self.board[1]
-            elif mouse_pos[0] <= 640:
-                self.board[2] = char if self.board[2] == 0 else self.board[2]
-        elif mouse_pos[1] <= 420:
-            if mouse_pos[0] <= 200:
-                self.board[3] = char if self.board[3] == 0 else self.board[3]
-            elif mouse_pos[0] <= 420:
-                self.board[4] = char if self.board[4] == 0 else self.board[4]
-            elif mouse_pos[0] <= 640:
-                self.board[5] = char if self.board[5] == 0 else self.board[5]
-        elif mouse_pos[1] <= 640:
-            if mouse_pos[0] <= 200:
-                self.board[6] = char if self.board[6] == 0 else self.board[6]
-            elif mouse_pos[0] <= 420:
-                self.board[7] = char if self.board[7] == 0 else self.board[7]
-            elif mouse_pos[0] <= 640:
-                self.board[8] = char if self.board[8] == 0 else self.board[8]
+        colon = int(mouse_pos[0] // 220)
+        row = int(mouse_pos[1] // 220)
+        self.board[row][colon] = char if self.board[row][colon] == 0 else self.board[row][colon]
 
-    def give_pos(self,square,text_width,text_height):
-        if square == 0:
-            return (100-text_width,100-text_height)
-        elif square == 1:
-            return (320-text_width,100-text_height)
-        elif square == 2:
-            return (540-text_width,100-text_height)
-        elif square == 3:
-            return (100-text_width,320-text_height)
-        elif square == 4:
-            return (320-text_width,320-text_height)
-        elif square == 5:
-            return (540-text_width,320-text_height)
-        elif square == 6:
-            return (100-text_width,540-text_height)
-        elif square == 7:
-            return (320-text_width,540-text_height)
-        elif square == 8:
-            return (540-text_width,540-text_height)
+    def check_square(self,mouse_pos):
+       colon = int(mouse_pos[0] // 220)
+       row = int(mouse_pos[1] // 220)
+       return (self.board[row][colon] != 0)
+  
+        
+    
+
+    def give_pos(self,row,colon,text_width,text_height):
+       x = ((colon * 220) + 100) - text_width
+       y = ((row * 220) + 100) - text_height
+       return (x,y)
--- main.py
+++ main.py
@@ -6,8 +6,8 @@ player1 = gameplayer.Player("o")
 player2 = gameplayer.Player("x")
 playerstate = True
 
-rect_color = (44,157,153)
-background_color = (64,135,132)
+RECT_COLOR = (44,157,153)
+BACKGROUND_COLOR = (64,135,132)
 
 pygame.init()
 
@@ -15,31 +15,35 @@ font = pygame.font.SysFont("arial",200)
 screen = pygame.display.set_mode(board.screen_size, 0, 32)
 
 
-screen.fill(background_color)
+screen.fill(BACKGROUND_COLOR)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         if event.type == pygame.MOUSEBUTTONDOWN:
+            if board.check_square(event.pos):
+                continue
             if playerstate:
                 board.change_square(event.pos, player1.give_char())
+                playerstate = False
             else:
                 board.change_square(event.pos, player2.give_char())
-            playerstate = not playerstate
+                playerstate = True
 
 
     #Drawing nine square on the screen
     for y in range(0,board.screen_size[1],220):
         for x in range(0,board.screen_size[0],220):
-            pygame.draw.rect(screen, rect_color, ((x,y),(200,200)))
+            pygame.draw.rect(screen, RECT_COLOR, ((x,y),(200,200)))
 
-    for i in range(0,9):
-        if board.board[i] == player1.give_char():
-            character = font.render(player1.give_char(), True, (255,226,190))
-            screen.blit(character, board.give_pos(i,character.get_width()//2,character.get_height()//2))
-        elif board.board[i] == player2.give_char():
-            character = font.render(player2.give_char(), True, (88,89,90))
-            screen.blit(character, board.give_pos(i,character.get_width()//2,character.get_height()//2))
-                
+    for row in range(0,3):
+        for colon in range(0,3):
+            if board.board[row][colon] == player1.give_char():
+                character = font.render(player1.give_char(), True, (98,102,104))
+                screen.blit(character, board.give_pos(row,colon,character.get_width()//2,character.get_height()//2))
+            elif board.board[row][colon] == player2.give_char():
+                character = font.render(player2.give_char(), True, (237,221,181))
+                screen.blit(character, board.give_pos(row,colon,character.get_width()//2,character.get_height()//2))
+
     pygame.display.update()
